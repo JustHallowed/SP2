8b7626d1535e16b5c4921e4edb23ef65c923548f
Undeadarchangel <yukanggogogo@gmail.com>
2020-02-17 00:54:53 +0800

    Update SceneText.cpp

--- Source/SceneText.cpp
+++ Source/SceneText.cpp
@@ -281,7 +281,53 @@ void SceneText::RenderMesh(Mesh* mesh, bool enableLight)
 
 void SceneText::RenderSkybox()
 {
-
+	modelStack.PushMatrix();
+		///scale, translate, rotate 
+		modelStack.Translate(-50.f, 0.f, 0.f);
+		modelStack.Scale(100.f, 100.f, 100.f);
+		modelStack.Rotate(90.f, 0.f, 1.f, 0.f);
+		RenderMesh(meshList[GEO_LEFT], false);
+	modelStack.PopMatrix();
+	modelStack.PushMatrix();
+		///scale, translate, rotate 
+		modelStack.Translate(50.f, 0.f, 0.f);
+		modelStack.Scale(100.f, 100.f, 100.f);
+		modelStack.Rotate(-90.f, 0.f, 1.f, 0.f);
+		RenderMesh(meshList[GEO_RIGHT], false);
+	modelStack.PopMatrix();
+	modelStack.PushMatrix();
+		///scale, translate, rotate 
+		modelStack.Translate(0.f, 50.f, 0.f);
+		modelStack.Scale(100.f, 100.f, 100.f);
+		modelStack.Rotate(90.f, 1.f, 0.f, 0.f);
+		modelStack.PushMatrix();
+			modelStack.Rotate(90.f, 0.f, 0.f, 1.f);
+			RenderMesh(meshList[GEO_TOP], false);
+		modelStack.PopMatrix();
+	modelStack.PopMatrix();
+	modelStack.PushMatrix();
+		///scale, translate, rotate 
+		modelStack.Translate(0.f, -50.f, 0.f);
+		modelStack.Scale(100.f, 100.f, 100.f);
+		modelStack.Rotate(-90.f, 1.f, 0.f, 0.f);
+		modelStack.PushMatrix();
+		modelStack.Rotate(90.f, 0.f, 0.f, 1.f);
+		RenderMesh(meshList[GEO_BOTTOM], false);
+		modelStack.PopMatrix();
+		modelStack.PopMatrix();
+	modelStack.PushMatrix();
+		///scale, translate, rotate 
+		modelStack.Translate(0.f, 0.f, -50.f);
+		modelStack.Scale(100.f, 100.f, 100.f);
+		RenderMesh(meshList[GEO_FRONT], false);
+	modelStack.PopMatrix();
+	modelStack.PushMatrix();
+		///scale, translate, rotate 
+		modelStack.Translate(0.f, 0.f, 50.f);
+		modelStack.Scale(100.f, 100.f, 100.f);
+		modelStack.Rotate(180.f, 0.f, 1.f, 0.f);
+		RenderMesh(meshList[GEO_BACK], false);
+	modelStack.PopMatrix();
 }
 
 void SceneText::RenderText(Mesh* mesh, std::string text, Color color)
