1c78d7b09484cc2aface4a5d4445910bef3860df
SRHD-999 <193541T@mymail.nyp.edu.sg>
2020-02-25 16:44:57 +0800

    Matrix Encryption
    (semi-working)

--- Source/Crypto.cpp
+++ Source/Crypto.cpp
@@ -11,36 +11,35 @@ void Encryptor::Encrypt(std::string& name, int& score, int complexity){
 			void(remove("LocalStorage/Key.secret"));
 			break;
 		}
-		case 3: { //Matrix encryption for name
-			//size_t strLen = name.length();
-			//short *keyMatrix = new short[strLen * strLen], *subjMatrix = new short[strLen], *resultMatrix = new short[strLen];
-			//std::string keyStr = "GYBNQKURPHJLAMCTGYBNQKURPHJLAMCTGYBNQKURPHJLAMCT";
-			//int k = 0;
-			//for(size_t i = 0; i < strLen * strLen; ++i){
-			//	keyMatrix[i] = rand() % 3;
-			//	//keyMatrix[i] = (keyStr[k]) % 65;
-			//	//++k;
-			//}
-			//for(size_t i = 0; i < strLen; ++i){
-			//	subjMatrix[i] = short(name[i]);
-			//	resultMatrix[i] = 0;
-			//}
-			//std::string tempStr = "";
-			//for(size_t r = 0; r < strLen; ++r){
-			//	for(size_t c = 0; c < strLen; ++c){
-			//		resultMatrix[r] += keyMatrix[r * strLen + c] * subjMatrix[r];
-			//		tempStr += char(resultMatrix[r]);
-			//	}
-			//}
-			//name = tempStr;
-			//std::ofstream key("LocalStorage/Key.secret");
-			//for(size_t i = 0; i < strLen; ++i){
-			//	key << (i ? "\n" : "") << resultMatrix[i];
-			//}
-			//key.close();
-			//delete[] keyMatrix;
-			//delete[] subjMatrix;
-			//delete[] resultMatrix;
+		case 3: { //Matrix encryption (based on Hill Cipher) for name
+			if(name.length() > 1){
+				reverse(name.begin(), name.end());
+				short keyMatrix[4], subjMatrix[2], resultMatrix[2]{0, 0};
+				keyMatrix[0] = rand() % 10; //0 - 9
+				keyMatrix[3] = rand() % 10; //0 - 9
+				do{
+					keyMatrix[1] = rand() % 9 + 1; //1 - 9
+					keyMatrix[2] = rand() % 9 + 1; //1 - 9
+				} while(keyMatrix[0] * keyMatrix[3] - keyMatrix[1] * keyMatrix[2] == 0); //AD - BC = 0
+				for(short i = 0; i < 2; ++i){
+					subjMatrix[i] = short(name[i]);
+				}
+				for(short r = 0; r < 2; ++r){
+					for(short c = 0; c < 2; ++c){
+						resultMatrix[r] += keyMatrix[r * 2 + c] * subjMatrix[c];
+					}
+				}
+				std::string tempStr = "";
+				for(short i = 0; i < signed(name.length()); ++i){
+					tempStr += (i < 2 ? char(resultMatrix[i]) : name[i]);
+				}
+				name = tempStr;
+				std::ofstream key("LocalStorage/Key.secret");
+				for(short i = 0; i < 4; ++i){
+					key << (i ? "\n" : "") << keyMatrix[i];
+				}
+				key.close();
+			}
 		}
 		default: { //Normal encryption for score
 			if(!set){
@@ -68,7 +67,47 @@ Decryptor::Decryptor(): dCode{0, 0, 0}{}
 void Decryptor::Decrypt(std::string& name, int& score, int dType){
 	switch(dType){
 		case 0: return;
-		case 2: { //Matrix decryption for name
+		case 2: { //Matrix decryption (based on Hill Cipher) for name
+			std::ifstream key("LocalStorage/Key.secret");
+			std::string line;
+			short keyMatrix[4], subjMatrix[2], resultMatrix[2]{0, 0};
+			for(short i = 0; i < 2; ++i){
+				subjMatrix[i] = short(name[i]);
+			}
+			try{
+				if(key.is_open()){
+					short index = 0;
+					while(getline(key, line)){
+						keyMatrix[index] = short(stoi(line));
+						++index;
+					}
+					key.close();
+				} else{
+					throw("Decryption failed.");
+				}
+			} catch(std::string errorMsg){
+				printf(errorMsg.c_str());
+			}
+
+			//Get inverse
+			keyMatrix[0] += keyMatrix[3];
+			keyMatrix[3] = keyMatrix[0] - keyMatrix[3];
+			keyMatrix[0] -= keyMatrix[3];
+			keyMatrix[1] = -keyMatrix[1];
+			keyMatrix[2] = -keyMatrix[2];
+
+			for(short r = 0; r < 2; ++r){
+				for(short c = 0; c < 2; ++c){
+					resultMatrix[r] += keyMatrix[r * 2 + c] * subjMatrix[c];
+				}
+			}
+			std::string tempStr = "";
+			for(short i = 0; i < signed(name.length()); ++i){
+				tempStr += (i < 2 ? char(resultMatrix[i]) : name[i]);
+			}
+			name = tempStr;
+			remove("LocalStorage/Key.secret");
+			reverse(name.begin(), name.end());
 		}
 		case 1: { //Normal decryption for score
 			std::ifstream codes("LocalStorage/Codes.secret");
