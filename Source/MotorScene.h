2f9584e89b0e3f39347a6c9144b1080a7c8f3739
loopyboopy <61051680+loopyboopy@users.noreply.github.com>
2020-02-26 16:47:30 +0800

    Animation functions

--- Source/MotorScene.h
+++ Source/MotorScene.h
@@ -89,7 +89,7 @@ class MotorScene final: public Scene{
 	ShaderManager* shMan;
 	unsigned m_vertexArrayID;
 	void InitMeshes(), CreateInstances(), RenderLight(), RenderMeshOnScreen(Mesh*, float, float, float, float, int, int), RenderSkybox(bool), RenderTextOnScreen(Mesh*, std::string, Color, float, float, float, int, int);
-	void InitLight() const, RenderParticle(Mesh*, GLfloat) const, RenderMesh(Mesh*, bool) const, RenderAnimation(Mesh*, std::string, Color) const, RenderText(Mesh*, std::string, Color) const, renderObject(Object* obj);
+	void InitLight() const, RenderParticle(Mesh*, GLfloat) const, RenderMesh(Mesh*, bool) const, RenderAnimation(Mesh*, int) const,RenderAnimationOnScreen(Mesh*,int,float,float,float,int,int), RenderText(Mesh*, std::string, Color) const, renderObject(Object* obj);
 	void createPlatforms(), createUFOs(), createRobot1(), createVehicles(), createRobot2(), createRobot3();
 	void npcCheck(int instance, const char* audioFileName);
 	void carCheck(int instance, const char* audioFileName);
